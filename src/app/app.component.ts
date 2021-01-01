81ab4c2b5d4f54114ee37707789024a83280ccfe
gagankumarrr <gkpippal@gmail.com>
2021-01-01 14:37:20 +0530

    Services in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {UserDataService} from './user-data.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'services';
+  name='';
+  constructor( private user:UserDataService){
+    console.warn(this.user.getData())
+    let data=this.user.getData();
+    this.name=data.name;
+  }
 }
